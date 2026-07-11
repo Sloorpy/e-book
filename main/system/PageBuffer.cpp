0061c79843d28c53ba5354fc89ce2a26eb901da3
Sloorpy <133083793+Sloorpy@users.noreply.github.com>
2026-07-11 22:12:20 +0300

    PageBuffer(refactor): simplify class (for now) add buffering later

--- main/system/PageBuffer.cpp
+++ main/system/PageBuffer.cpp
@@ -4,61 +4,22 @@
 PageBuffer::PageBuffer(std::weak_ptr<BookFs> book_fs, const uint16_t chapter_num) :
     _chapter_num(chapter_num),
     _chapter_size(book_fs.lock()->chapter_size(chapter_num)),
-    _book_fs(book_fs),
-    _current_offset(0)
+    _book_fs(book_fs)
 {
 }
 
-bool PageBuffer::has_next() const
+std::vector<uint8_t> PageBuffer::read_chunk(const uint64_t start, const uint64_t end)
 {
-    return _current_offset < _chapter_size;
-}
-
-bool PageBuffer::has_prev() const
-{
-    return _current_offset > BUFFER_SIZE;
-}
-
-std::vector<uint8_t> PageBuffer::next()
-{
-    if (!has_next()) {
-        throw std::runtime_error("PageBuffer iterator is at end");
+    if (end <= start) {
+        throw std::runtime_error("You cannot read from end to start");
     }
 
     std::shared_ptr<BookFs> book = _book_fs.lock();
-    const std::vector<uint8_t> bytes = book->read_page(_chapter_num, BUFFER_SIZE, _current_offset);
-
-    _current_offset += bytes.size();
-    if (bytes.size() != BUFFER_SIZE && has_next()) {
-        throw std::runtime_error("PageBuffer fail to read BUFFER_SIZE bytes");
-    }
-
+    const std::vector<uint8_t> bytes = book->read_page(_chapter_num, end - start, start);
     return bytes;
 }
 
-std::vector<uint8_t> PageBuffer:: prev()
+uint64_t PageBuffer::size()
 {
-    if (!has_prev()) {
-        throw std::runtime_error("PageBuffer iterator is at start");
-    }
-
-    std::shared_ptr<BookFs> book = _book_fs.lock();
-
-    const uint64_t previous_page = previous_page_offset();
-    const std::vector<uint8_t> bytes = book->read_page(_chapter_num, BUFFER_SIZE, previous_page);
-
-    _current_offset = previous_page + bytes.size();
-    if (bytes.size() != BUFFER_SIZE && has_prev()) {
-        throw std::runtime_error("PageBuffer fail to read BUFFER_SIZE bytes");
-    }
-
-    return bytes;
-}
-
-uint64_t PageBuffer::previous_page_offset() const
-{
-    if (_current_offset >= BUFFER_SIZE * 2) {
-        return _current_offset - BUFFER_SIZE * 2;
-    }
-    return 0;
+    return _chapter_size;
 }
--- main/system/PageBuffer.hpp
+++ main/system/PageBuffer.hpp
@@ -11,17 +11,13 @@ public:
     ~PageBuffer() = default;
 
 public:
-    bool has_next() const;
-    bool has_prev() const;
-    std::vector<uint8_t> next();
-    std::vector<uint8_t> prev();
+    std::vector<uint8_t> read_chunk(const uint64_t start, const uint64_t end);
+    uint64_t size();
 
 private:
-    uint64_t previous_page_offset() const;
-private:
-    static constexpr size_t BUFFER_SIZE = 2048;
+    //static constexpr size_t BUFFER_SIZE = 2048;
+    //uint64_t _current_offset;
     const uint16_t _chapter_num;
     const uint64_t _chapter_size;
     std::weak_ptr<BookFs> _book_fs;
-    uint64_t _current_offset;
 };
